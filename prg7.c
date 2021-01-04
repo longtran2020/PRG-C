b73c2835216eb8431f7d0e91cc80bf8c7ff3afd0
longtran2020 <76939821+longtran2020@users.noreply.github.com>
2021-01-04 22:26:20 +0100

    Add files via upload

---
+++ prg7.c
@@ -0,0 +1,19 @@
+# include  < stdlib.h >  
+# include  < stdio.h >  
+
+
+int soucet(int x, int y)
+{
+    return x + y
+}
+
+int main()
+{
+
+    printf("%i\n", soucet(6, 5));
+    printf("%i\n", soucet(9, 5));
+    printf("%i\n", soucet(1, 5));
+    printf("%i\n", soucet(3, 5));
+
+    return 0;
+}
