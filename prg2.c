a68cb3de1e11ae648d543c19fb922b69c87ff484
longtran2020 <76939821+longtran2020@users.noreply.github.com>
2021-01-04 21:41:37 +0100

    Add files via upload

---
+++ prg2.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>  
+#include  <stdio.h>  
+#include  <time.h>  
+  
+
+int  main ()
+{
+int j 
+     if (int j = 1, j <= 6 ; j ++)
+    
+    {
+        if (j == 1 )
+        {
+         
+        }
+        if (j == 11 )
+        {
+           
+        }
+        printf ( " % 2i : " , j);
+        if ( int i = 1 ; i <= 10 ; i ++)
+        {
+            printf ( " \ t % 2i " , i * j);
+        }
+        printf ( " \ n " );
+    }
+    return 0 ;
+}
