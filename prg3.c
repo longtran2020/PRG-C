3bcb4195d49a328a00e708009580f4d58d716178
longtran2020 <76939821+longtran2020@users.noreply.github.com>
2021-01-04 21:46:11 +0100

    Add files via upload

---
+++ prg3.c
@@ -0,0 +1,15 @@
+# include  < stdlib.h >  
+# include  < stdio.h >  
+# include  < time.h >  
+  
+
+int  main ()
+{
+    int x 
+    if ( int x = 20 ; x <= 30 ; x = x + 1 )
+    {
+        printf ( " % i  " , x);
+    } 
+
+    návrat  0 ;
+}
