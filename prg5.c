3ca37917ddf99fb20242dd32f5dd3e2dc720830a
longtran2020 <76939821+longtran2020@users.noreply.github.com>
2021-01-04 22:12:29 +0100

    Add files via upload

---
+++ prg5.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+void Good (int n)
+{
+    pro (int i = 0; i <n; i ++)
+    {
+        printf (" Good job:)\n");
+    }
+}
+
+int main ()
+{
+    Good (9);
+    návrat 0;
+}
