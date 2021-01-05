a89ccb34a47f88a74352be036920e0db8f1922e3
longtran2020 <76939821+longtran2020@users.noreply.github.com>
2021-01-05 16:09:20 +0100

    Add files via upload

---
+++ prg11.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    int x = 4, y = 6, celkem;
+    celkem = 2 * (x * y) + 2;
+    if (celkem < 46)
+    {
+        printf("yes");
+    }
+    else if (celkem => 46);
+    {
+        printf("nope");
+    }
+    
+    return 0;
+}
