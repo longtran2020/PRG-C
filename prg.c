1d17b8ca74de864abe28da5c79bd781933a9eff6
longtran2020 <76939821+longtran2020@users.noreply.github.com>
2021-01-05 09:31:54 +0100

    Add files via upload

--- prg.c
+++ prg.c
@@ -3,11 +3,14 @@
 #include <stdio.h>  
 #include <time.h> 
 
-int main() {
-
-printf("                                     čísla pod sebou\n");
+int main() 
+{
+ printf("čísla pod sebou\n"); 
     int i;
     for(i=1; i<=33; i++){
        printf("%i\n", i);
+
+    return 0;
 }
- return 0;
+
+
