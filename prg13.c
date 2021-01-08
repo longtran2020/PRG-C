14484a405d9375aeb93ad4a69523f9e89ce7cdd8
longtran2020 <76939821+longtran2020@users.noreply.github.com>
2021-01-08 13:16:51 +0100

    Add files via upload

---
+++ prg13.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    int x, result;
+    
+    x = 5;
+    result = square( x );
+    printf("%d squared is %d\n", x, result);
+
+    return 0;
+}
